a1a0718e97e60b0db4c148ce8904f1dedf3e80e3
zumbalove974 <omranedoo@gmail.com>
2023-03-12 07:09:41 +0100

    epsg en 4326

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const ITOWNS_GPX_PARSER_OPTIONS = { in: { crs: 'EPSG:2975' }, out: { crs: 'EPSG:2975', mergeFeatures: true } };
+const ITOWNS_GPX_PARSER_OPTIONS = { in: { crs: 'EPSG:4326' }, out: { crs: 'EPSG:4326', mergeFeatures: true } };
 
 // Retrieve the view container
 const viewerDiv = document.getElementById('viewerDiv');
 
 // Define the view geographic extent
 itowns.proj4.defs(
-    'EPSG:2975',
-    '+proj=utm +zone=40 +south +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs'
+    'EPSG:4326',
+    '+proj=longlat + ellps=WGS84 + datum=WGS84 + no_defs'
 );
-const viewExtent = new itowns.Extent(
-    'EPSG:2975',
-    300000.0, 400000.0,
-    7630000.0, 7700000.0,
+const viewExtent = new itowns.Extent(///////////regarder istowns.Extent prend quelles genre de coords
+    'EPSG:4326',
+    6140203.0, 6219351.0,
+    -2373832.0, -2439479.0,
 );
 
 // Define the camera initial placement
@@ -29,10 +29,10 @@ const view = new itowns.PlanarView(viewerDiv, viewExtent, {
 
 // Define the source of the ortho-images
 const sourceOrtho = new itowns.WMSSource({
-    url: "https://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/r/wms",
+    url: "https://wxs.ign.fr/ortho/geoportail/r/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities",
     name: "HR.ORTHOIMAGERY.ORTHOPHOTOS",
     format: "image/png",
-    crs: 'EPSG:2975',
+    crs: 'EPSG:4326',
     extent: viewExtent,
 });
 // Create the ortho-images ColorLayer and add it to the view
@@ -41,10 +41,10 @@ view.addLayer(layerOrtho);
 
 // Define the source of the dem data
 const sourceDEM = new itowns.WMSSource({
-    url: "https://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/r/wms",
+    url: "https://wxs.ign.fr/ortho/geoportail/r/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities",
     name: "ELEVATION.ELEVATIONGRIDCOVERAGE.HIGHRES",
     format: "image/x-bil;bits=32",
-    crs: 'EPSG:2975',
+    crs: 'EPSG:4326',
     extent: viewExtent,
 });
 // Create the dem ElevationLayer and add it to the view
@@ -61,25 +61,33 @@ itowns.Fetcher.xml('./assets/GrandRaid.gpx')
 
 function displayPath(vertices) {
     let coordList = [];
+    console.log(view.referenceCrs);
     for (let i = 0; i < vertices.length / 3; i++) {
         coordList.push(new itowns.Coordinates('EPSG:4326', vertices[i * 3], vertices[i * 3 + 1], vertices[i * 3 + 2]).as(view.referenceCrs).toVector3());
-        console.log(new itowns.Coordinates('EPSG:4326', vertices[i * 3], vertices[i * 3 + 1], vertices[i * 3 + 2]).x * 180 / 3.14)
+        //console.log(new itowns.Coordinates('EPSG:4326', vertices[i * 3], vertices[i * 3 + 1], vertices[i * 3 + 2] + 10).as(view.referenceCrs).toVector3());
     }
 
     const curve = new itowns.THREE.CatmullRomCurve3(coordList, false);
+
+    let geometry = new itowns.THREE.TubeGeometry(curve, coordList.length * 10, 10, 8, false);
+    /*
+    geometry = new BufferGeometry().fromGeometry(geometry);
+    
     const points = curve.getPoints(500);
     //console.log(points);
     //console.log(view.camera.camera3D);
 
-    const geometry = new itowns.THREE.BufferGeometry().setFromPoints(points);
+    const geometry = new itowns.THREE.BufferGeometry().setFromPoints(points);*/
     const material = new itowns.THREE.LineBasicMaterial({ color: 0xff0000 });
-
+/*
     // Create the final object to add to the scene
     const curveObject = new itowns.THREE.Line(geometry, material);
     console.log(curveObject);
-
+*/
     //view.rende.setClearColor(0x00ff00, 0);
+    const curveObject = new itowns.THREE.Mesh(geometry, material);
+    console.log(curveObject);
 
     view.scene.add(curveObject);
-    //view.camera.camera3D.position.set(points[0].x, points[0].y, points[0].z);
+    //view.camera.camera3D.position.set(-7274487.5, 10000000, 19.999799728393555);
 };
