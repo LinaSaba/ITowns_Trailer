afe7bf5d874cf55ce12ad951eb5b0da17eaad2f2
omranedoo <omranedoo@gmail.com>
2023-03-17 12:26:48 +0100

    j'ai trouver comment obtenir les coordonnées geographiques a partir du gpx

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const viewerDiv = document.getElementById('viewerDiv');
 
 // Define the view geographic extent
 itowns.proj4.defs(
-    'EPSG:4326',
-    '+proj=longlat + ellps=WGS84 + datum=WGS84 + no_defs'
+    'EPSG:2975',
+    '+proj=utm +zone=40 +south +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs'
 );
-const viewExtent = new itowns.Extent(///////////regarder istowns.Extent prend quelles genre de coords
-    'EPSG:4326',
-    6140203.0, 6219351.0,
-    -2373832.0, -2439479.0,
+const viewExtent = new itowns.Extent(
+    'EPSG:2975',
+    300000.0, 400000.0,
+    7630000.0, 7700000.0,
 );
 
 const ITOWNS_GPX_PARSER_OPTIONS = { in: { crs: 'EPSG:4326' }, out: { crs: 'EPSG:4326', mergeFeatures: true } };
@@ -29,10 +29,10 @@ const view = new itowns.PlanarView(viewerDiv, viewExtent, {
 
 // Define the source of the ortho-images
 const sourceOrtho = new itowns.WMSSource({
-    url: "https://wxs.ign.fr/ortho/geoportail/r/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities",
+    url: "https://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/r/wms",
     name: "HR.ORTHOIMAGERY.ORTHOPHOTOS",
     format: "image/png",
-    crs: 'EPSG:4326',
+    crs: 'EPSG:2975',
     extent: viewExtent,
 });
 // Create the ortho-images ColorLayer and add it to the view
@@ -41,10 +41,10 @@ view.addLayer(layerOrtho);
 
 // Define the source of the dem data
 const sourceDEM = new itowns.WMSSource({
-    url: "https://wxs.ign.fr/ortho/geoportail/r/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities",
+    url: "https://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/r/wms",
     name: "ELEVATION.ELEVATIONGRIDCOVERAGE.HIGHRES",
     format: "image/x-bil;bits=32",
-    crs: 'EPSG:4326',
+    crs: 'EPSG:2975',
     extent: viewExtent,
 });
 // Create the dem ElevationLayer and add it to the view
@@ -67,7 +67,8 @@ function updatePointScale(renderer, scene, camera) {
 let vertices;
 let index = 0;
 let runner;
-let dPosition = new itowns.THREE.Vector3(0, 0, 0);
+let group = new itowns.THREE.Group();
+let waypoints = [];
 
 var waypointGeometry = new itowns.THREE.BoxGeometry(1, 1, 80);
 var waypointMaterial = new itowns.THREE.MeshBasicMaterial({ color: 0xffffff });
@@ -76,6 +77,13 @@ view.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, function () {
     console.info('Globe initialized');
     itowns.Fetcher.xml('./assets/diag.gpx')
         .then((gpx) => {
+            let i = 0;
+            console.log(gpx.activeElement.children[0].children[1].children);
+            for (let i = 0; i < 7133; i++) {
+                waypoints.push(gpx.activeElement.children[0].children[1].children[i].attributes)
+            }
+
+            console.log(gpx.activeElement.children[0].children[1].children[i].attributes.lat.value);
             itowns.GpxParser.parse(gpx, {
                 in: {
                     crs: 'EPSG:4326',
@@ -111,44 +119,33 @@ view.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, function () {
                                     waypoint.updateMatrix();
                                     mesh.add(waypoint);
                                     waypoint.updateMatrixWorld();
-
-                                    // Supposons que vous avez un objet mesh appelé `parentMesh` auquel vous avez ajouté d'autres objets
-                                    // et que vous voulez convertir les coordonnées d'un de ces objets appelé `childObject`
-
-                                    // Récupérez la matrice de transformation globale de l'objet parent
-                                    const parentMatrix = mesh.matrixWorld;
-
-                                    // Appliquez la matrice de transformation globale de l'objet parent à la matrice de transformation locale de l'objet enfant
-                                    const childMatrix = waypoint.matrixWorld.clone();
-                                    childMatrix.premultiply(parentMatrix);
-
-                                    // Créez un vecteur contenant les coordonnées de l'objet enfant dans son propre système de coordonnées locales
-                                    const childPosition = new itowns.THREE.Vector3();
-                                    childPosition.setFromMatrixPosition(waypoint.matrix);
-
-                                    // Convertissez les coordonnées de l'objet enfant en coordonnées globales en appliquant la matrice de transformation globale combinée
-                                    childPosition.applyMatrix4(childMatrix);
-
-                                    console.log(childPosition);
                                 }
                             }
                         });
+
                         view.scene.add(mesh);
                         view.notifyChange();
 
                         //console.log(mesh);
 
                         let geometryS = new itowns.THREE.SphereGeometry(2000, 320, 320);
-                        let materialS = new itowns.THREE.MeshBasicMaterial({ color: 0xffffff });
+                        let materialS = new itowns.THREE.MeshBasicMaterial({ color: 0xff00ff });
                         runner = new itowns.THREE.Mesh(geometryS, materialS);
                         //runner.position.copy(new itowns.THREE.Vector3(349061.88680361793, 7666676.953710422, 2000));
                         //runner.lookAt(mesh.worldToLocal(new itowns.THREE.Vector3()));
                         //runner.onBeforeRender = updatePointScale;
                         runner.updateMatrix();
-                        runner.position.set(3862.255004731298, 25093.030992013402, 2441);
+
                         //runner.position.set(vertices[index * 3], vertices[index * 3 + 1], vertices[index * 3 + 2]);
+                        /*group.updateMatrixWorld();
+                        group.position.copy(mesh.position);
+                        group.add(runner);*/
                         view.scene.add(runner);
                         runner.updateMatrixWorld();
+
+                        //view.scene.add(group);
+                        console.log(mesh);
+                        //console.log(group);
 
                         //view.camera.camera3D.position.set(3862.255004731298, 25093.030992013402, 2501);
                         //view.camera.camera3D.lookAt(runner.position)
@@ -167,13 +164,13 @@ function animate() {
 
     requestAnimationFrame(animate)
 
-    index += 3;
+    index += 1;
 
     //runner.lookAt(runner.worldToLocal(new itowns.THREE.Vector3()));
     runner.updateMatrix();
-    runner.position.fromArray(vertices, index);
+    //runner.position.fromArray(vertices, index);
     //runner.position.set(3862.255004731298, 2441, 25093.030992013402);
-    //runner.position.set(vertices[index * 3], vertices[index * 3 + 1], vertices[index * 3 + 2]);
+    runner.position.copy(new itowns.Coordinates('EPSG:4326', waypoints.lat.value, waypoints.lon.value, 2000).as(view.referenceCrs));
     runner.updateMatrixWorld();
 
     //console.log(runner.position)
@@ -184,4 +181,3 @@ function animate() {
 function render() {
     view.mainLoop.gfxEngine.renderer.render(view.scene, view.camera.camera3D);
 }
-
